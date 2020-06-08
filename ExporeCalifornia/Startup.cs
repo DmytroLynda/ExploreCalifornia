ae14fb53020ee426a0f283f961c62583d7bf8b6e
Dmytro Lynda <DmytroLynda@users.noreply.github.com>
2020-06-08 21:39:34 +0200

    Add the ability to change the exception page for developers to the page for customer and vice versa

--- ExporeCalifornia/Startup.cs
+++ ExporeCalifornia/Startup.cs
@@ -1,15 +1,17 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using System;
 
 namespace ExporeCalifornia {
     public class Startup {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration) {
+            Configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
@@ -17,12 +19,12 @@ namespace ExporeCalifornia {
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+            
+            if (Configuration.GetValue<bool>("EnableDeveloperExceptions"))
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseExceptionHandler("/error.html");
 
             app.Use(async (context, next) =>
             {
