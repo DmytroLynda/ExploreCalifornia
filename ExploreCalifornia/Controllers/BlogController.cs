2cb85f6c356eee474f058c2d78710af4271a169e
Dmytro Lynda <DmytroLynda@users.noreply.github.com>
2020-06-09 16:01:38 +0200

    Add simple blog controller

---
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExploreCalifornia.Controllers {
+    public class BlogController : Controller {
+        // GET: /<controller>/
+        public IActionResult Index() {
+            return new ContentResult { Content = "Blog post" };
+        }
+    }
+}
