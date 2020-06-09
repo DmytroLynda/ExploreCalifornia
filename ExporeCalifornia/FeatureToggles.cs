91aa2701466ec553c6d661952a4c068de7d5487d
Dmytro Lynda <DmytroLynda@users.noreply.github.com>
2020-06-09 10:58:53 +0200

    Replace the way of get feature toggles through dependency injection

---
+++ ExporeCalifornia/FeatureToggles.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExporeCalifornia {
+    public class FeatureToggles {
+        public bool DeveloperExceptions { get; set; }
+    }
+}
--- ExporeCalifornia/Startup.cs
+++ ExporeCalifornia/Startup.cs
@@ -15,13 +15,20 @@ namespace ExporeCalifornia {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services) {
+            services.AddTransient(x => new FeatureToggles
+            {
+                DeveloperExceptions = Configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions")
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+        public void Configure(
+            IApplicationBuilder app,
+            IWebHostEnvironment env,
+            FeatureToggles features) {
             app.UseExceptionHandler("/error.html");
-            
-            if (Configuration.GetValue<bool>("FeatureToggles:DeveloperExceptions"))
+
+            if (features.DeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
