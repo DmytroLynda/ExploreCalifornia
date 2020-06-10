a6f003f037347bf8abf530be5e7cad3093a7d886
Dmytro Lynda <DmytroLynda@users.noreply.github.com>
2020-06-10 14:58:12 +0200

    Fix the routing of post method

--- ExploreCalifornia/Controllers/BlogController.cs
+++ ExploreCalifornia/Controllers/BlogController.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace ExploreCalifornia.Controllers {
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ExploreCalifornia.Controllers
+{
     [Route("blog")]
-    public class BlogController : Controller {
+    public class BlogController : Controller
+    {
         [Route("")]
-        public IActionResult Index() {
+        public IActionResult Index()
+        {
             return View();
         }
 
-        [Route("blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
-        public IActionResult Post(int year, int month, string key) {
+        [Route(@"blog/{year:min(2000)}/{month:range(1,12)}/{key}")]
+        public IActionResult Post(int year, int month, string key) 
+        {
+            ViewBag.Title = "My blog post";
+            ViewBag.Posted = DateTime.Now;
+            ViewBag.Author = "Jess Chadwick";
+            ViewBag.Body = "This is a great blog post, don't you think?";
+
             return View();
         }
     }
